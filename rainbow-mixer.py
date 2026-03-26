b21dad14fd0ac22f5b39e39fee7baa1289606a56
Gabriel D. <gaboale166@gmail.com>
2026-03-26 11:44:41 -0400

    added sliders

--- rainbow-mixer.py
+++ rainbow-mixer.py
@@ -1,6 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
-    pass
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-flet.app(target = main, assets_dir = "assets")
+    r_slider = ft.Slider(key = "r", min = 0, max = 255, label = "R")
+    g_slider = ft.Slider(key = "g", min = 0, max = 255, label = "G")
+    b_slider = ft.Slider(key = "b", min = 0, max = 255, label = "B")
+
+    page.add(r_slider, g_slider, b_slider)
+
+ft.app(target = main, assets_dir = "assets")
