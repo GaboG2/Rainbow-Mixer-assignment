eaef7da9bf3c3f420f810b0c0e9c456797dd4865
Gabriel D. <gaboale166@gmail.com>
2026-03-27 15:50:18 -0400

    Page color now changes to the sliders

--- rainbow-mixer.py
+++ rainbow-mixer.py
@@ -5,11 +5,15 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     def change_bg_color(e):
-        pass
+        r = int(r_slider.value)
+        g = int(g_slider.value)
+        b = int(b_slider.value)
 
-    r_slider = ft.Slider(key = "r", min = 0, max = 255, on_change = change_bg_color)
-    g_slider = ft.Slider(key = "g", min = 0, max = 255, on_change = change_bg_color)
-    b_slider = ft.Slider(key = "b", min = 0, max = 255, on_change = change_bg_color)
+        page.bgcolor = f"#{r:02x}{g:02x}{b:02x}"
+
+    r_slider = ft.Slider(key = "r", min = 0, max = 255, value = 0, on_change = change_bg_color)
+    g_slider = ft.Slider(key = "g", min = 0, max = 255, value = 0, on_change = change_bg_color)
+    b_slider = ft.Slider(key = "b", min = 0, max = 255, value = 0, on_change = change_bg_color)
 
     page.add(
         ft.Row(controls = [ft.Text("R"), r_slider], alignment = page.vertical_alignment),
