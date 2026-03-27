859952d728558f755c90b20a957ffb21b48c6420
Gabriel D. <gaboale166@gmail.com>
2026-03-27 12:41:58 -0400

    Update rainbow-mixer.py

--- rainbow-mixer.py
+++ rainbow-mixer.py
@@ -4,6 +4,9 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    def change_bg_color(e):
+        page.bgcolor = f"#{r_slider.value:02x}{g_slider.value:02x}{b_slider.value:02x}"
+
     r_slider = ft.Slider(key = "r", min = 0, max = 255, label = "R")
     g_slider = ft.Slider(key = "g", min = 0, max = 255, label = "G")
     b_slider = ft.Slider(key = "b", min = 0, max = 255, label = "B")
